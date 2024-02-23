e7ee4c0159a81e4cc7a71f43ff9e145ecb71de7e
Jun <coolch0916@gmail.com>
2024-02-23 14:56:55 +0900

    Fix duplicate value error

--- src/modules/like/entity/like.entity.ts
+++ src/modules/like/entity/like.entity.ts
@@ -1,25 +1,22 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Book } from 'src/modules/book/entity/book.entity';
 import { User } from 'src/modules/user/entity/user.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 
 @Entity('likes')
 @ObjectType({ description: 'Like' })
 export class Like {
-  @PrimaryGeneratedColumn({ type: 'int' })
-  id: number;
-
   @ManyToOne(() => User)
   user: User;
 
   @ManyToOne(() => Book)
   likedBook: Book;
 
-  @Column({ type: 'int' })
+  @PrimaryColumn({ type: 'int' })
   @Field(() => Number)
   userId: number;
 
-  @Column({ type: 'int' })
+  @PrimaryColumn({ type: 'int' })
   @Field(() => Number)
   likedBookId: number;
 }
